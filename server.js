15b29219d6db7fe483550c77199eade50c917728
Vladimir <v.a.evtushenko@yandex.ru>
2020-11-10 15:46:24 +0300

    updeate6

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const http=require("http")
 const fs=require("fs")
 const server=http.createServer((req,res)=>{
     
-    let body=fs.readFileSync(`./Lesson8/index.html`)
+    let index=fs.readFileSync(`./Lesson8/index.html`)
+    let css=fs.readFileSync(`./Lesson8/css.css`)
     
-    res.end(body)
+    let c=[index,css]
+    // c.forEach(element=>fs.readFileSync())
+    
+    res.end(`${c}`)
 })
 
 const port = process.env.PORT || 3040
