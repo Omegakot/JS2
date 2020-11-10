fdf1392a049e5ae545a55aae8274350ad33dfb46
Vladimir <v.a.evtushenko@yandex.ru>
2020-11-10 15:00:10 +0300

    update1
    update2
    
    
    update 4

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const http=require("http")
 const fs=require("fs")
 const server=http.createServer((req,res)=>{
-    let body=fs.readFileSync(`./111.jpg`)
-   
+    
+    let body=fs.readFileSync(`./Lesson8/index.html`)
+    
     res.end(body)
 })
 
 const port = process.env.PORT || 3040
 server.listen(port)
 
-server.listen(3040)
-console.log("Started")
+console.log(`Started on port ${port}`)
