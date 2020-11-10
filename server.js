1ee322d849fd26b77f5ca82f5b168b1f0c1cfda8
Vladimir <v.a.evtushenko@yandex.ru>
2020-11-11 02:52:15 +0300

    update

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const http= require("http")
 const fs=require("fs")
 const server=http.createServer((req,res)=>{
+    console.log(req.url)
+    const body=req.url==="/css.css"
+    ? fs.readFileSync(`./lesson8/css.css`)
+    :fs.readFileSync(`./lesson8/index.html`)
 
-    const body=req.url === `/css.css`
-
-? fs.readFileSync(`./Lesson8/css.css`)
-: fs.readFileSync(`./Lesson8/index.html`)
     res.end(body)
 })
 
-const port = process.env.PORT || 3040
+const port = process.env.PORT || 3050
 server.listen(port)
 
 console.log(`Started on port ${port}`)
