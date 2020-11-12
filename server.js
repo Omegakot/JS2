ebffba3095265afaa6b5fe61ae54867769361cd2
Vladimir <v.a.evtushenko@yandex.ru>
2020-11-12 22:09:17 +0300

    complete

--- server.js
+++ server.js
@@ -2,25 +2,32 @@ const http= require("http")
 const fs=require("fs")
 const server=http.createServer((req,res)=>{
 console.log(req.url)
-
-let body
-
-switch(req.url){
-    case `/css.css`:
-        body=fs.readFileSync(`./Lesson8/css.css`)
-    break
-    case `/`:
+const path =`./Lesson8`
+// switch(req.url){
+//     case `/css.css`:
+//         console.log("REQ1")
+//         body=fs.readFileSync(`./Lesson8/css.css`)
+//     break
+//     case `/`:
+//         console.log("REQ2")
+//     body=fs.readFileSync(`./Lesson8/index.html`)
+//     break
+//     case `/img2.jpg`:
+//         body=fs.readFileSync(`./Lesson8/img2.jpg`)
+//     break
+//     case `/img1.jpg`:
+//         body=fs.readFileSync(`./Lesson8/img1.jpg`)
+//     break  
+//     case `/script2.js`:
+//         body=fs.readFileSync(`./Lesson8/script2.js`)
+//     break        
+// }
+try {
+    console.log("Попытка")
+    body=fs.readFileSync(`${path}${req.url}`)
+}
+catch(err){
     body=fs.readFileSync(`./Lesson8/index.html`)
-    break
-    case `/img2.jpg`:
-        body=fs.readFileSync(`./Lesson8/img2.jpg`)
-    break
-    case `/img1.jpg`:
-        body=fs.readFileSync(`./Lesson8/img1.jpg`)
-    break  
-    case `/script2.js`:
-        body=fs.readFileSync(`./Lesson8/script2.js`)
-    break        
 }
     res.end(body)
 })
